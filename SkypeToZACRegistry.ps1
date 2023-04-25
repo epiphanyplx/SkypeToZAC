c5cd2aa1c13e59ddcfc4cf6ff49f2fe7ea0a7b9c
epiphanyplx <epiphanyplx@gmail.com>
2023-04-25 10:05:00 -0400

    Syntax

--- SkypeToZACRegistry.ps1
+++ SkypeToZACRegistry.ps1
@@ -16,7 +16,7 @@ Write-Host -ForegroundColor Green "Modifying Registry"
 New-Item "HKCU:\Software\Classes\skype\shell\open\command" -Force | Out-Null
 New-ItemProperty "HKCU:\Software\Classes\skype" -Name '(Default)' -Value 'URL:skype' -Force | Out-Null
 New-ItemProperty "HKCU:\Software\Classes\skype" -Name 'URL Protocol' -Force | Out-Null
-New-ItemProperty "HKCU:\Software\Classes\skype\shell\open\command" -Name '(Default)' -Value 'mshta vbscript:Execute("CreateObject(""Wscript.Shell"").Run ""powershell -NoLogo -ExecutionPolicy Bypass -Command """"& c:\SkypeToZAC\SkypeToZAC.ps1 -uri %1"""""", 0: window.close")' -Force | Out-Null
+New-ItemProperty "HKCU:\Software\Classes\skype\shell\open\command" -Name '(Default)' -Value 'mshta vbscript:Execute("CreateObject(""Wscript.Shell"").Run ""powershell -NoLogo -ExecutionPolicy Bypass -Command """"& "c:\SkypeToZAC\SkypeToZAC.ps1" -uri %1"""""", 0: window.close")' -Force | Out-Null
 
 
 #Make sure C:\SkypeToZAC folder exists
@@ -25,7 +25,7 @@ if (!(Test-Path C:\SkypeToZAC)) {New-Item -Type Directory 'C:\SkypeToZAC'}
 
 #Create Bat file
 Write-Host -ForegroundColor Green "Downloading .ps1 file"
-iwr -URI 'https://raw.githubusercontent.com/epiphanyplx/SkypeToZAC/master/SkypeToZAC.ps1' -outfile 'c:\SkypeToZAC\SkypeToZAC.ps1'
+Invoke-WebRequest -URI 'https://raw.githubusercontent.com/epiphanyplx/SkypeToZAC/master/SkypeToZAC.ps1' -outfile 'c:\SkypeToZAC\SkypeToZAC.ps1'
 
 #Create README File
 Write-Host -ForegroundColor Green "Creating README File"
